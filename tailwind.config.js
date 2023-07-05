56b93b4ccf09c615e7fa91c37cd7bdc98530338e
Alden  Sullivan <aw.sullivan17@gmail.com>
2023-07-05 18:21:12 -0400

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        bg: '#10100E',
+        text: '#E8E8D8',
+        'text2': '#89897B'
+      }
     },
   },
   plugins: [],
